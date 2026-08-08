9433992365d9480ce2f078e118300735a3425820
Divyanshu mishra <misradivyansh9@gmail.com>
2026-08-08 12:47:43 +0530

    Time: 3 ms (66.39%), Space: 15.1 MB (9.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,40 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        for(int i = 0; i < nums.size(); i++){
-
-            for(int j = i + 1; j < nums.size(); j++){
-
-                if(nums[i] + nums[j] == target){
-
-                    return {i, j};
-                }
+        map<int,int> mpp;
+        vector<int>arr;
+        int l=nums.size();
+        for(int i=0;i<l;i++){
+           
+            int diff=target-nums[i];
+            if(mpp.find(diff)!=mpp.end()){
+                arr.push_back(i);
+                arr.push_back(mpp[diff]);
+                return arr;
             }
+             mpp[nums[i]]=i;
         }
 
-        return {};
+        return arr;
+        
     }
 };
+
+//orlass Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+
+//         for(int i = 0; i < nums.size(); i++){
+
+//             for(int j = i + 1; j < nums.size(); j++){
+
+//                 if(nums[i] + nums[j] == target){
+
+//                     return {i, j};
+//                 }
+//             }
+//         }
+
+//         return {};
+//     }
+// };
