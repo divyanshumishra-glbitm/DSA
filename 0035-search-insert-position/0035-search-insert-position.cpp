6a0bc2ad7e2b498ebc9d4d82407aac724e0e638d
Divyanshu mishra <misradivyansh9@gmail.com>
2026-08-15 00:56:01 +0530

    Time: 0 ms (100%), Space: 13.6 MB (43.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        //u can do by stl also
-        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-
-        return lb;
-        
+        //u can implement lower bound code in it 
+        int ans=nums.size();
+        int low=0;
+        int high=ans-1;
+        int mid;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(target<=nums[mid]){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
