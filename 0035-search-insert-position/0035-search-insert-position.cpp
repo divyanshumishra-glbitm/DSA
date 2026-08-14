4968d698e7b0edde8a089c2a34ff150c878b4199
Divyanshu mishra <misradivyansh9@gmail.com>
2026-08-15 00:53:03 +0530

    Time: 0 ms (100%), Space: 13.8 MB (43.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        //u can do by stl also
+        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
 
-        // sorted array searching should be done using binary search
-        int start=0,length=nums.size(),end=length-1,mid;
-        while(start<=end){
-            mid=end+(start-end)/2;
-            if(target==nums[mid]){
-                return mid;
-            }else if(target<nums[mid]){
-                end=mid-1;
-            }else{
-                start=mid+1;
-            }
-                
-            
-
-
-        }
-        return start;
-
+        return lb;
+        
     }
-    
 };
