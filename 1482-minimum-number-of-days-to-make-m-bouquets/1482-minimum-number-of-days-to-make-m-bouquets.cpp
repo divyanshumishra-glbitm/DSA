770b849b515acd15cc6c953271067a08cc8b6e93
Divyanshu mishra <misradivyansh9@gmail.com>
2026-08-27 16:44:03 +0530

    Time: 32 ms (10.84%), Space: 70.6 MB (12.27%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,48 +1,50 @@
-class Solution {
-public:
-    bool possible(vector<int>& bloomDay, int m, int k, int day) {
-        int bouquets = 0;
-        int flowers = 0;
-
-        for (int i = 0; i < bloomDay.size(); i++) {
-
-            if (bloomDay[i] <= day) {
-                flowers++;
+class Solution { 
+public: 
+     
+    bool possible(vector<int>& bloomDay, int m, int k, int day){ 
+        int count = 0; 
+        int noOFb = 0; 
+        
+        for(int i = 0; i < bloomDay.size(); i++){ 
+            if(bloomDay[i] <= day){ 
+                count++; 
+            }else{ 
+                noOFb += (count / k); 
+                count = 0; 
             } 
-            else {
-                bouquets += flowers / k;
-                flowers = 0;
-            }
-        }
-
-        bouquets += flowers / k;
-
-        return bouquets >= m;
-    }
-
-    int minDays(vector<int>& bloomDay, int m, int k) {
-
-        int n = bloomDay.size();
-
-        // Not enough flowers
-        if ((long long)m * k > n) {
-            return -1;
-        }
-
-        int low = *min_element(bloomDay.begin(), bloomDay.end());
-        int high = *max_element(bloomDay.begin(), bloomDay.end());
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (possible(bloomDay, m, k, mid)) {
-                high = mid - 1; // Try fewer days
+        } 
+        
+        noOFb += (count / k); 
+ 
+        if(noOFb >= m) return true; 
+        else return false; 
+    } 
+     
+    int minDays(vector<int>& bloomDay, int m, int k) { 
+        int n = bloomDay.size(); 
+        
+        int minIndex = min_element(bloomDay.begin(), bloomDay.end()) - bloomDay.begin(); 
+        int start = bloomDay[minIndex]; 
+        
+        int maxIndex = max_element(bloomDay.begin(), bloomDay.end()) - bloomDay.begin(); 
+        int end = bloomDay[maxIndex]; 
+        
+        int mid; 
+        int ans = -1; 
+ 
+        if((long long)n < (long long)m * k) return -1; 
+ 
+        while(start <= end){ 
+            mid = start + (end - start) / 2; 
+ 
+            if(possible(bloomDay, m, k, mid) == true){ 
+                ans = mid; 
+                end = mid - 1; 
+            }else{ 
+                start = mid + 1; 
             } 
-            else {
-                low = mid + 1;  // Need more days
-            }
-        }
-
-        return low;
-    }
+        } 
+ 
+        return ans; 
+    } 
 };
