1c4c99d56bd19b0a482a8c5c9d2458c270893f6e
Divyanshu mishra <misradivyansh9@gmail.com>
2026-08-31 20:00:42 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.86%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,40 @@
-class Solution { 
-public: 
-    vector<int> majorityElement(vector<int>& nums) { 
- 
-        vector<int> arr; 
-        map<int,int>mpp; 
-        int n=nums.size(); 
-        int mm=(int)n/3 +1; 
- 
-        for(int i=0;i<n;i++){ 
-            mpp[nums[i]]++; 
- 
-            if(mpp[nums[i]]==mm) 
-                arr.push_back(nums[i]); 
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>arr;
+        int n=nums.size();
+
+        int count1=0;
+        int count2=0;
+        int element1=INT_MIN;
+        int element2=INT_MIN;
+
+
+        for(int i=0;i<n;i++){
+        if(count1==0&&nums[i]!=element2){
+            count1=1;
+            element1=nums[i];
+        }else if(count2==0&&nums[i]!=element1){
+            count2=1;
+            element2=nums[i];
+        }
+        else if(element1==nums[i]) count1++;
+        else if(element2==nums[i]) count2++;
+        else{
+            count1--,count2--;
+        }
+        }
+
+       count1=0,count2=0;
+       for(int i=0;i<n;i++){
+        if(element1==nums[i]) count1++;
+        if(element2==nums[i]) count2++;
+       }
+       int mini=(int)n/3+1;
+       if(count1>=mini ) arr.push_back(element1);
+       if(count2>=mini ) arr.push_back(element2);
+
+       return arr;
         
-        }  
-        return arr; 
-    } 
+    }
 };
