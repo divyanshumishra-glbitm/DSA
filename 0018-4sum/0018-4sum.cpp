34afd5366fe1b17920701b0e36a6a36264850f54
Divyanshu mishra <misradivyansh9@gmail.com>
2026-09-02 18:13:31 +0530

    Time: 22 ms (59.97%), Space: 17.6 MB (12.45%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,63 +2,44 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
-        int n = nums.size();
-        vector<vector<int>> ans;
+        //the optimal approach would be two pointer only
 
-        sort(nums.begin(), nums.end());
+      int n=nums.size();
+      sort(nums.begin(),nums.end());
+      vector<vector<int>> ans;
 
-        for(int i = 0; i < n - 3; i++) {
+      for(int i=0;i<n-3;i++){
+        if(i>0&&nums[i-1]==nums[i]) continue;
+        for(int j=i+1;j<n-2;j++){
+            if(j>i+1&&nums[j-1]==nums[j]) continue;
 
-            if(i > 0 && nums[i - 1] == nums[i])
-                continue;
+            int k=j+1;
+            int l=n-1;
 
-            for(int j = i + 1; j < n - 2; j++) {
-
-                if(j > i + 1 && nums[j - 1] == nums[j])
-                    continue;
-
-                int k = j + 1;
-                int l = n - 1;
-
-                while(k < l) {
-
-                    long long result = (long long)nums[i]
-                                      + nums[j]
-                                      + nums[k]
-                                      + nums[l];
-
-                    if(result < target) {
+            while(k<l){
+                long long result=(long long)nums[i]+nums[j]+nums[k]+nums[l];
+                if(result<target){
+                    k++;
+                }else if(result>target){
+                    l--;
+                }else{
+                    vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while(k<l&&nums[k-1]==nums[k]){
                         k++;
                     }
-                    else if(result > target) {
+                    while(k<l&&nums[l+1]==nums[l]){
                         l--;
-                    }
-                    else {
-
-                        vector<int> temp = {
-                            nums[i],
-                            nums[j],
-                            nums[k],
-                            nums[l]
-                        };
-
-                        ans.push_back(temp);
-
-                        // Move both pointers
-                        k++;
-                        l--;
-
-                        // Skip duplicates
-                        while(k < l && nums[k] == nums[k - 1])
-                            k++;
-
-                        while(k < l && nums[l] == nums[l + 1])
-                            l--;
                     }
                 }
             }
-        }
 
-        return ans;
+        }
+      }
+
+
+       return ans;
     }
 };
